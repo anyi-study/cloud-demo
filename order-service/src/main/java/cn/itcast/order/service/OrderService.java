70d30a402450e46aea5f2832c77729cc0cc7abff
CodeJ <jx523644@163.com>
2024-05-08 20:53:09 +0800

    添加eureka

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -19,7 +19,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         //2.利用restTemplate调用user-service查询用户
-        String url = "http://localhost:8081/user/"+order.getUserId();
+        String url = "http://cloud-user-service/user/"+order.getUserId();
         //发送请求，获取用户数据
         User user = restTemplate.getForObject(url, User.class);
         //封装数据回去
